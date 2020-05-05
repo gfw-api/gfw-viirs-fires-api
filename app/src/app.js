34059b9abf42ea346a6adaf137ef52bdb75d9fd5
Tiago Garcia <tiagojsag@gmail.com>
2020-05-05 08:20:28 +0200

    Improvements to eslint rule exceptions

--- app/src/app.js
+++ app/src/app.js
@@ -53,7 +53,6 @@ app.use(function* handleErrors(next) {
     this.response.type = 'application/vnd.api+json';
 });
 
-// eslint-disable-next-line import/no-unresolved
 const cache = require('lru-cache')({
     maxAge: 30000 // global max age
 });
